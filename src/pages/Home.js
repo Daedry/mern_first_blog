9a7dfacbcd84e000df875a3a085d193af7f6d60e
MyUsername <romamargine@gmail.com>
2022-09-15 23:34:39 +0200

    Add pages

---
+++ src/pages/Home.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="mb-20">
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">
+        Welcome to my blog
+      </h1>
+      <p className="mx-auto leading-relaxed text-base">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta at,
+        nostrum enim maxime quam officiis officia accusamus, voluptas fuga fugit
+        laudantium commodi placeat atque magnam tenetur mollitia delectus
+        amet consectetur adipisicing elit. Soluta at,
+        nostrum enim maxime quam officiis officia accusamus, voluptas fuga fugit
+        laudantium commodi placeat atque magnam tenetur mollitia delectus
+        provident beatae!
+      </p>
+
+      <p className="mx-auto leading-relaxed text-base">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta at,
+        amet consectetur adipisicing elit. Soluta at,
+        nostrum enim maxime quam officiis officia accusamus, voluptas fuga fugit
+        laudantium commodi placeat atque magnam tenetur mollitia delectus
+        nostrum enim maxime quam officiis officia accusamus, voluptas fuga fugit
+        laudantium commodi placeat atque magnam tenetur mollitia delectus
+        provident beatae!
+      </p>
+
+      <p className="mx-auto leading-relaxed text-base">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta at,
+        nostrum enim maxime quam officiis officia accusamus, voluptas fuga fugit
+        amet consectetur adipisicing elit. Soluta at,
+        nostrum enim maxime quam officiis officia accusamus, voluptas fuga fugit
+        laudantium commodi placeat atque magnam tenetur mollitia delectus
+        laudantium commodi placeat atque magnam tenetur mollitia delectus
+        provident beatae!
+      </p>
+
+      <p className="mx-auto leading-relaxed text-base">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta at,
+        nostrum enim maxime quam officiis officia accusamus, voluptas fuga fugit
+        amet consectetur adipisicing elit. Soluta at,
+        nostrum enim maxime quam officiis officia accusamus, voluptas fuga fugit
+        laudantium commodi placeat atque magnam tenetur mollitia delectus
+        laudantium commodi placeat atque magnam tenetur mollitia delectus
+        provident beatae!
+      </p>
+    </div>
+  );
+};
+
+export default Home;
